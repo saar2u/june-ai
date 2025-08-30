abfa95235202cda3818915c9b914566d02526ce1
saar2u <saar.upadhyay@gmail.com>
2025-08-31 02:43:23 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,15 +5,14 @@ import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  base: "/", // <- ensure assets load correctly on Amplify
   build: {
-    outDir: "build",
+    outDir: "build",       // matches Amplify baseDirectory
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
-    port: "4028",
+    port: 4028,
     host: "0.0.0.0",
     strictPort: true,
     allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
